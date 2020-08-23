ee1c74ec8aa39fabad99a61c85923a5ecd557fcc
Alejandro de Pablos <adpabloslopez@gmail.com>
2020-08-23 08:59:41 -0500

    Prepare imports and config for js files
    Config for js scripts added and also preparing index.js file with some imports which are gonna be needed.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// styles
+import "assets/css/bootstrap.min.css";
+import "assets/scss/paper-kit.scss?v=1.2.0";
+import "assets/demo/demo.css?v=1.2.0";
+// pages
+import LandingPage from "views/LandingPage.js";
+// others
+import './i18n';
 
 ReactDOM.render(
   <React.StrictMode>
